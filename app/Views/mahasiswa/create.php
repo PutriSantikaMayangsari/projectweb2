fee6d9622021979be52d1187a88495ba551de292
PutriSantikaMayangsari <83493403+PutriSantikaMayangsari@users.noreply.github.com>
2022-09-13 14:30:15 +0700

    Create create.php

---
+++ app/Views/mahasiswa/create.php
@@ -0,0 +1,17 @@
+<div class="p-4">
+    <form>
+        <div class="form-group" p-4>
+            <label for="npm">NPM</label>
+            <input type="text" name="npm" class="form-control" id="npm" placeholder="Enter npm">
+        </div>
+        <div class="form-group">
+            <label for="nama">Nama</label>
+            <input type="text" name="nama" class="form-control" id="nama" placeholder="Enter nama">
+        </div>
+        <div class="form-group">
+            <label for="alamat">Alamat</label>
+            <input type="text" name="alamat" class="form-control" id="alamat" placeholder="Enter alamat">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
